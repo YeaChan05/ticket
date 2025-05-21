d9c2040d86b9757bceec2761611df419431abc95
YeaChan05 <qkenrdl05@gmail.com>
2025-05-21 23:01:54 +0900

    feat: Hall 엔티티 추가 및 Show 엔티티에 연관관계 매핑 추가

--- domain/domain-show/src/main/java/entity/Hall.java
+++ domain/domain-show/src/main/java/entity/Hall.java
@@ -2,29 +2,21 @@ package entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.yechan.entity.BaseEntity;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Show extends BaseEntity {
+public class Hall {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "show_id")
+    @Column(name="hall_id")
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "hall_id")
-    private Hall hall;
 }

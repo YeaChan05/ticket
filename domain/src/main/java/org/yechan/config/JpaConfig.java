adbe90cd03eb69610f202e2c8d7e6cf187ec87e2
YeaChan05 <qkenrdl05@gmail.com>
2025-05-22 13:41:10 +0900

    feat: JPA 설정 및 Querydsl 지원 추가

---
+++ domain/src/main/java/org/yechan/config/JpaConfig.java
@@ -0,0 +1,22 @@
+package org.yechan.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "org.yechan.repository") // JPA Repository 패키지 지정
+public class JpaConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}

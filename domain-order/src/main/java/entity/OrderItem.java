e8cd208c9f2f9c022334da95188f9a24102276a3
YeaChan05 <qkenrdl05@gmail.com>
2025-05-25 16:00:04 +0900

    refactor: OrderItem 클래스 BaseEntity 상속 및 연관 관계 수정
    OrderItem 클래스가 BaseEntity를 상속하도록 변경하고, 필드와 연관 관계 어노테이션 수정.

--- domain-order/src/main/java/entity/OrderItem.java
+++ domain-order/src/main/java/entity/OrderItem.java
@@ -12,12 +12,13 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.yechan.entity.BaseEntity;
 
 @Entity
 @Table(name = "order_items")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class OrderItem {
+public class OrderItem extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_item_id")
@@ -28,15 +29,15 @@ public class OrderItem {
     private Order order;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
+    @JoinColumn(name = "seat_id")
     private Seat seat;
-    
+
     @Column(name = "seat_name", nullable = false)//TODO 2025 05 22 20:58:27 : custom value and converter
     private String seatName; // 좌석 이름 (예: A열 12번)
-    
+
     @Column(name = "seat_grade", nullable = false)
     private String seatGrade; // 좌석 등급 (VIP, R, S 등)
-    
+
     @Column(name = "price", nullable = false)
     private Integer price; // 좌석 가격
 }

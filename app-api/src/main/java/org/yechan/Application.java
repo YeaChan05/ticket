cf9333a5ec65f577163400b3ea5683753feadbbc
YeaChan05 <qkenrdl05@gmail.com>
2025-05-27 17:47:14 +0900

    refactor: 기본 패키지 스캔에 'config' ˆ패키지를 추가

--- app-api/src/main/java/org/yechan/Application.java
+++ app-api/src/main/java/org/yechan/Application.java
@@ -3,7 +3,7 @@ package org.yechan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.yechan", "repository", "entity"})
+@SpringBootApplication(scanBasePackages = {"org.yechan", "repository", "entity", "config"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

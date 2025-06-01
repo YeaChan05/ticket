ed4eef6766102df388f9d6dfe025d01613085bde
YeaChan05 <qkenrdl05@gmail.com>
2025-06-01 17:49:54 +0900

    feat: UserRepository에 이메일 및 이름 중복 검사 메서드 추가
    - existsByEmail, existsByName 메서드 추가
    - @Query 및 @Param 어노테이션 사용

--- domain-user/src/main/java/org/yechan/repository/JpaUserRepository.java
+++ domain-user/src/main/java/org/yechan/repository/JpaUserRepository.java
@@ -1,9 +1,14 @@
 package org.yechan.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.yechan.entity.User;
 
 public interface JpaUserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    @Query("SELECT 1 FROM User u WHERE u.email = :email")
+    boolean existsByEmail(@Param("email") String email);
+
+    @Query("SELECT 1 FROM User u WHERE u.name = :name")
+    boolean existsByName(String name);
 }

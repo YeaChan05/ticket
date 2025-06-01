1f463d33fd560e22619d2170964715205c8cd73f
YeaChan05 <qkenrdl05@gmail.com>
2025-06-01 22:22:23 +0900

    feat: ApiResponse 클래스 확장 및 유틸리티 메서드 추가
    - JsonInclude 어노테이션 추가로 null 필드 제외 처리
    - 메시지 필드 추가 및 관련 생성자 오버로드
    - 성공(sucess), 에러(error) 생성 유틸리티 메서드 추가로 응답 생성 간소화

--- internal/internal-web/src/main/java/org/yechan/config/response/ApiResponse.java
+++ internal/internal-web/src/main/java/org/yechan/config/response/ApiResponse.java
@@ -1,13 +1,34 @@
 package org.yechan.config.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
     private final String status;
     private final T data;
+    private final String message;
     private final LocalDateTime timestamp=LocalDateTime.now();
+
+    public ApiResponse(String status, T data) {
+        this.status = status;
+        this.data = data;
+        this.message = null;
+    }
+
+    public ApiResponse(String status, String message) {
+        this.status = status;
+        this.data = null;
+        this.message = message;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>("SUCCESS", data);
+    }
+
+    public static <T> ApiResponse<T> error(String status, String message) {
+        return new ApiResponse<>(status, message);
+    }
 }

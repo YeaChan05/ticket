5dd1942e782a7b0e1448f88dce5b4876bf43140a
YeaChan05 <qkenrdl05@gmail.com>
2025-06-01 17:49:17 +0900

    feat: UserRepository 기능 추가
    - 이메일 및 이름 중복 검사 메서드 추가.
    - 사용자 정보 저장(insertUser) 메서드 구현.

--- domain-user/src/main/java/org/yechan/repository/UserRepository.java
+++ domain-user/src/main/java/org/yechan/repository/UserRepository.java
@@ -2,9 +2,28 @@ package org.yechan.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.yechan.entity.User;
 
 @Repository
 @RequiredArgsConstructor
-public class UserRepository{
+public class UserRepository {
     private final JpaUserRepository jpaUserRepository;
+
+    public boolean existsByEmail(final String email) {
+        return jpaUserRepository.existsByEmail(email);
+    }
+
+    public boolean existByName(final String name) {
+        return jpaUserRepository.existsByName(name);
+    }
+
+    public void insertUser(final String name, final String email, final String password, final String phone) {
+        User user = User.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .phone(phone)
+                .build();
+        jpaUserRepository.save(user);
+    }
 }

b15fd7665e9a832ef5033b2685191c2b524360ed
YeaChan05 <qkenrdl05@gmail.com>
2025-06-01 22:37:17 +0900

    fix: UserErrorCode 코드 및 메시지 수정
    - NAME_DUPLICATED의 코드값 수정
    - PHONE_DUPLICATED 추가

--- app-api/src/main/java/org/yechan/error/UserErrorCode.java
+++ app-api/src/main/java/org/yechan/error/UserErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public enum UserErrorCode implements ErrorCode {
     EMAIL_DUPLICATED("EMAIL-001", "이미 사용중인 이메일입니다."),
-    NAME_DUPLICATED("EMAIL-001", "중복된 이름입니다.");
+    NAME_DUPLICATED("EMAIL-002", "중복된 이름입니다."),
+    PHONE_DUPLICATED("EMAIL-003", "이미 사용중인 연락처입니다.");
 
     private final String code;
     private final String message;

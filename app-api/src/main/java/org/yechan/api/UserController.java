e5399775a07849d2a67e6bfb1a2f0652f3605042
YeaChan05 <qkenrdl05@gmail.com>
2025-06-01 17:50:13 +0900

    feat: UserController 추가
    - 회원가입 요청 처리 엔드포인트 구현
    - UserRegisterUseCase를 통해 비즈니스 로직 호출
    - "/api/v1/users/sign-up" 경로로 회원가입 요청 처리

---
+++ app-api/src/main/java/org/yechan/api/UserController.java
@@ -0,0 +1,26 @@
+package org.yechan.api;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yechan.api.port.UserRegisterUseCase;
+import org.yechan.dto.request.UserRegisterRequest;
+import org.yechan.dto.response.RegisterSuccessResponse;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public record UserController(
+        UserRegisterUseCase userRegisterUseCase
+) {
+    @PostMapping("/sign-up")
+    public RegisterSuccessResponse registerUser(@Validated @RequestBody UserRegisterRequest userRegisterRequest) {
+        return userRegisterUseCase.registerUser(
+                userRegisterRequest.name(),
+                userRegisterRequest.email(),
+                userRegisterRequest.password(),
+                userRegisterRequest.phone()
+        );
+    }
+}

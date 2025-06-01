fea6464d2630996e804c3c1ccbf7b09b84f60c24
YeaChan05 <qkenrdl05@gmail.com>
2025-06-01 16:41:07 +0900

    feat: ApiResponse 클래스 추가
    글로벌 응답 데이터 표준화를 위한 ApiResponse 제네릭 클래스 생성. 상태, 데이터, 타임스탬프 포함.

---
+++ internal/internal-web/src/main/java/org/yechan/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package org.yechan.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private final String status;
+    private final T data;
+    private final LocalDateTime timestamp=LocalDateTime.now();
+}

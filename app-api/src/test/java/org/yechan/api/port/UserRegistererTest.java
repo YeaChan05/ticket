16d8900110dbe7d05bda7f838309ea5be626ac74
YeaChan05 <qkenrdl05@gmail.com>
2025-06-02 16:55:15 +0900

    test: UserRegistererTest에 전화번호 중복 예외 테스트 추가 및 기존 테스트 수정
    - 전화번호 중복 상황에 대한 예외 테스트 케이스 추가
    - lenient() 및 existsByPhoneNumber 활용으로 테스트 로직 개선
    - 기존 테스트 이름과 로직을 상황에 맞게 수정하여 가독성 및 명확성 향상

--- app-api/src/test/java/org/yechan/api/port/UserRegistererTest.java
+++ app-api/src/test/java/org/yechan/api/port/UserRegistererTest.java
@@ -4,6 +4,7 @@ package org.yechan.api.port;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import autoparams.AutoParams;
@@ -66,10 +67,25 @@ class UserRegistererTest {
 
     @Test
     @AutoParams
-    void 두_예외상황이_둘다_발생하는_상황의_경우_email_예외가_먼저_발생한다(String name, String email, String password, String phone) {
+    void 이미_존재하는_전화번호로_회원가입을_시도하면_예외가_발생한다(String name, String email, String password, String phone) {
         // given
-        when(userRepository.existsByEmail(anyString())).thenReturn(true);
-        when(userRepository.existByName(anyString())).thenReturn(true);
+        when(userRepository.existsByEmail(anyString())).thenReturn(false);
+        when(userRepository.existByName(anyString())).thenReturn(false);
+        when(userRepository.existsByPhoneNumber(anyString())).thenReturn(true);
+
+        // when & then
+        assertThatThrownBy(() -> userRegisterer.registerUser(name, email, password, phone))
+                .isInstanceOf(UserException.class)
+                .hasMessage("User with phone " + phone + " already exists.");
+    }
+
+    @Test
+    @AutoParams
+    void 세_예외상황이_둘다_발생하는_상황의_경우_email_예외가_먼저_발생한다(String name, String email, String password, String phone) {
+        // given
+        lenient().when(userRepository.existsByEmail(email)).thenReturn(true);
+        lenient().when(userRepository.existByName(name)).thenReturn(true);
+        lenient().when(userRepository.existsByPhoneNumber(phone)).thenReturn(true);
 
         // when & then
         assertThatThrownBy(() -> userRegisterer.registerUser(name, email, password, phone))

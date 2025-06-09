e68ba0b086208fed030e80ea54927a24a1f298a2
YeaChan05 <qkenrdl05@gmail.com>
2025-06-09 20:25:09 +0900

    test: 로그인 시도 시 등록되지 않은 email에 대한 USER 예외 처리 추가

--- app-api/src/test/java/org/yechan/api/issueToken/POST_spec.java
+++ app-api/src/test/java/org/yechan/api/issueToken/POST_spec.java
@@ -1,7 +1,9 @@
 package org.yechan.api.issueToken;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.yechan.api.fixture.UserFixture.generateEmail;
 import static org.yechan.api.fixture.UserFixture.generateUserRegisterRequest;
+import static org.yechan.api.fixture.UserFixture.generateUsername;
 
 import java.net.URI;
 import org.junit.jupiter.api.Test;
@@ -9,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.yechan.api.config.IntegrationTest;
 import org.yechan.config.response.ApiResponse;
+import org.yechan.config.response.ErrorResponse;
 import org.yechan.dto.request.IssueTokenRequest;
+import org.yechan.dto.request.UserRegisterRequest;
 
 @IntegrationTest
 public class POST_spec {
@@ -73,5 +77,38 @@ public class POST_spec {
         // Assert
         assertThat(response).isNotNull();
         assertThat(response.getData()).isNotNull();
+    }
+
+    @Test
+    void 로그인_시도_시_등록되지_않은_email이면_USER_예외가_발생한다(
+            @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var userRegisterRequest = new UserRegisterRequest(
+                generateUsername(),
+                generateEmail(),
+                "securep!21Assword",
+                generateEmail()
+        );
+        var email = userRegisterRequest.email();
+        var password = userRegisterRequest.password();
+
+        // not registering the user
+        IssueTokenRequest request = new IssueTokenRequest(
+                email,
+                password
+        );
+
+        // Act
+        var response = client.postForObject(
+                URI.create("/api/v1/auth/issueToken"),
+                request,
+                ErrorResponse.class
+        );
+
+        // Assert
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo("USER-001");
+        assertThat(response.getMessage()).isEqualTo("사용자를 찾을 수 없습니다.");
     }
 }

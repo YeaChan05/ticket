ad01047073fc9ae90054d0d111ab971db2f3189b
YeaChan05 <qkenrdl05@gmail.com>
2025-06-09 18:09:22 +0900

    test: UserFixture 메서드 접근 제한자 수정

--- app-api/src/test/java/org/yechan/api/fixture/UserFixture.java
+++ app-api/src/test/java/org/yechan/api/fixture/UserFixture.java
@@ -2,35 +2,21 @@ package org.yechan.api.fixture;
 
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
-import org.yechan.dto.request.UserRegisterRequest;
 
 public class UserFixture {
-    public static UserRegisterRequest generateUserRegisterRequest() {
-        String uuid = UUID.randomUUID().toString();
-        var username = generateUsername(uuid);
+    private static final String uuid = UUID.randomUUID().toString();
 
-        var email = generateEmail(uuid);
 
-        var phone = generatePhone();
-
-        return new UserRegisterRequest(
-                username,
-                email,
-                "Password123!",// 중복되어도 상관 없음
-                phone
-        );
-    }
-
-    private static String generateEmail(final String uuid) {
+    public static String generateEmail() {
         String localPart = uuid.replace("-", "");
         return localPart + "@test.com";
     }
 
-    private static String generateUsername(final String uuid) {
+    public static String generateUsername() {
         return uuid.substring(0, 8);
     }
 
-    private static String generatePhone() {
+    public static String generatePhone() {
         int middle = ThreadLocalRandom.current().nextInt(1000, 10000);
         int last = ThreadLocalRandom.current().nextInt(1000, 10000);
         return String.format("010-%d-%d", middle, last);

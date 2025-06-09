3117d35412f70a71934e54c540d39a8e5dec7ea0
YeaChan05 <qkenrdl05@gmail.com>
2025-06-09 17:38:02 +0900

    fix: ErrorResponse 필드 수정 및 기본 생성자 추가
    - jackson 직렬화를 위한 응답 객체 생성 방식 수정

--- internal/internal-web/src/main/java/org/yechan/config/response/ErrorResponse.java
+++ internal/internal-web/src/main/java/org/yechan/config/response/ErrorResponse.java
@@ -2,12 +2,14 @@ package org.yechan.config.response;
 
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ErrorResponse {
-    private final String status;
-    private final String message;
-    private final LocalDateTime timestamp = LocalDateTime.now();
+    private String status;
+    private String message;
+    private LocalDateTime timestamp = LocalDateTime.now();
 
     public ErrorResponse(String status, String message) {
         this.status = status;

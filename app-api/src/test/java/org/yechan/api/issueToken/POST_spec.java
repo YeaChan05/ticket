2bcc65c6d0007d1c7f32e6b9ee6dd5ecbe86cf52
YeaChan05 <qkenrdl05@gmail.com>
2025-06-09 18:27:31 +0900

    test: 토큰 발급 테스트 통과

--- app-api/src/test/java/org/yechan/api/issueToken/POST_spec.java
+++ app-api/src/test/java/org/yechan/api/issueToken/POST_spec.java
@@ -1,6 +1,7 @@
 package org.yechan.api.issueToken;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.yechan.api.fixture.UserFixture.generateUserRegisterRequest;
 
 import java.net.URI;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.yechan.api.config.IntegrationTest;
 import org.yechan.config.response.ApiResponse;
 import org.yechan.dto.request.IssueTokenRequest;
-import org.yechan.dto.request.UserRegisterRequest;
 
 @IntegrationTest
 public class POST_spec {
@@ -18,15 +18,12 @@ public class POST_spec {
             @Autowired TestRestTemplate client
     ) {
         // Arrange
-        String email = "user@test.com";
-        String password = "securep!21Assword";
-
-        String name="testUser";
-        String phone = "010-1234-5678";
-
+        var userRegisterRequest = generateUserRegisterRequest();
+        var email = userRegisterRequest.email();
+        var password = userRegisterRequest.password();
         client.postForObject(
                 "/api/v1/users/sign-up",
-                new UserRegisterRequest(name, email, password, phone),
+                userRegisterRequest,
                 ApiResponse.class
         );
 
@@ -45,5 +42,36 @@ public class POST_spec {
         // Assert
         assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo("SUCCESS");
+    }
+
+    @Test
+    void 로그인_시도_시_등록된_email과_password로_요청하면_JWT_토큰이_반환된다(
+            @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var userRegisterRequest = generateUserRegisterRequest();
+        var email = userRegisterRequest.email();
+        var password = userRegisterRequest.password();
+        client.postForObject(
+                "/api/v1/users/sign-up",
+                userRegisterRequest,
+                ApiResponse.class
+        );
+
+        IssueTokenRequest request = new IssueTokenRequest(
+                email,
+                password
+        );
+
+        // Act
+        var response = client.postForObject(
+                URI.create("/api/v1/auth/issueToken"),
+                request,
+                ApiResponse.class
+        );
+
+        // Assert
+        assertThat(response).isNotNull();
+        assertThat(response.getData()).isNotNull();
     }
 }

7210a753aa759be155a64105c4521322fe0b2490
YeaChan05 <qkenrdl05@gmail.com>
2025-06-09 20:26:08 +0900

    feat: 예외 처리 프로세스 추가

--- app-api/src/main/java/org/yechan/api/IssueUserTokenController.java
+++ app-api/src/main/java/org/yechan/api/IssueUserTokenController.java
@@ -3,13 +3,20 @@ package org.yechan.api;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yechan.dto.request.IssueTokenRequest;
 import org.yechan.dto.response.TokenHolder;
+import org.yechan.error.UserErrorCode;
+import org.yechan.error.exception.UserException;
+import org.yechan.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-public record IssueUserTokenController() {
+public record IssueUserTokenController(UserRepository userRepository) {
     @PostMapping("/issueToken")
-    public TokenHolder issueUserToken() {
+    public TokenHolder issueUserToken(IssueTokenRequest request) {
+        if(!userRepository.existsByEmail(request.email())){
+            throw new UserException("제공된 이메일을 가진 사용자는 존재하지 않습니다.", UserErrorCode.USER_NOT_FOUND);
+        }
         return new TokenHolder("Token issued successfully");
     }
 }

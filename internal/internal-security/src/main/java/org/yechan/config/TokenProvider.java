3ed1b9c627a1fbeb88220134da201e100dc0e103
YeaChan05 <qkenrdl05@gmail.com>
2025-06-10 13:42:32 +0900

    feat: 토큰 발급기 추가

---
+++ internal/internal-security/src/main/java/org/yechan/config/TokenProvider.java
@@ -0,0 +1,38 @@
+package org.yechan.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenProvider {
+    private final Key key;
+    private final long accessExpiration;
+
+    public TokenProvider(SecurityConfigurationProperties properties) {
+        this.accessExpiration = properties.jwt().accessExpiration();
+        byte[] keyBytes = Base64.getDecoder().decode(properties.jwt().secret());
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public TokenHolder createAccessToken(String subject, Map<String, ?> claims) {
+        var date = new Date();
+        long now = date.getTime();
+        Date validity = new Date(now + this.accessExpiration * 1000);
+
+        var accessToken = Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(date)
+                .setClaims(claims)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setExpiration(validity)
+                .compact();
+        return new TokenHolder(accessToken);
+    }
+
+}

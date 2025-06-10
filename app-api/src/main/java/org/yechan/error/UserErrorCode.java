c418321a3d1db74f13d963ae39b8229a9b4dca14
YeaChan05 <qkenrdl05@gmail.com>
2025-06-10 13:42:53 +0900

    feat: 에러 코드 추가

--- app-api/src/main/java/org/yechan/error/UserErrorCode.java
+++ app-api/src/main/java/org/yechan/error/UserErrorCode.java
@@ -9,7 +9,8 @@ public enum UserErrorCode implements ErrorCode {
     EMAIL_DUPLICATED("EMAIL-001", "이미 사용중인 이메일입니다."),
     NAME_DUPLICATED("EMAIL-002", "중복된 이름입니다."),
     PHONE_DUPLICATED("EMAIL-003", "이미 사용중인 연락처입니다."),
-    USER_NOT_FOUND("USER-001", "사용자를 찾을 수 없습니다."),;
+    USER_NOT_FOUND("USER-001", "사용자를 찾을 수 없습니다."),
+    PASSWORD_MISMATCH("USER-002", "비밀번호가 일치하지 않습니다.");
 
     private final String code;
     private final String message;

550d7730e36a355d57c773d73814d2da01a92b01
YeaChan05 <qkenrdl05@gmail.com>
2025-06-10 13:40:17 +0900

    feat: 토큰 발급 usecase 리펙터링 및 요청 객체 @RequestBody 어노테이션 추가

--- app-api/src/main/java/org/yechan/api/IssueUserTokenController.java
+++ app-api/src/main/java/org/yechan/api/IssueUserTokenController.java
@@ -1,22 +1,18 @@
 package org.yechan.api;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yechan.api.port.IssueTokenUseCase;
+import org.yechan.config.TokenHolder;
 import org.yechan.dto.request.IssueTokenRequest;
-import org.yechan.dto.response.TokenHolder;
-import org.yechan.error.UserErrorCode;
-import org.yechan.error.exception.UserException;
-import org.yechan.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-public record IssueUserTokenController(UserRepository userRepository) {
+public record IssueUserTokenController(IssueTokenUseCase issueTokenUseCase) {
     @PostMapping("/issueToken")
-    public TokenHolder issueUserToken(IssueTokenRequest request) {
-        if(!userRepository.existsByEmail(request.email())){
-            throw new UserException("제공된 이메일을 가진 사용자는 존재하지 않습니다.", UserErrorCode.USER_NOT_FOUND);
-        }
-        return new TokenHolder("Token issued successfully");
+    public TokenHolder issueUserToken(@RequestBody IssueTokenRequest request) {
+        return issueTokenUseCase.issueToken(request);
     }
 }

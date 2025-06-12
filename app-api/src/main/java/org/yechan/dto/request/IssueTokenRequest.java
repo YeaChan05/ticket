d755c2006b87c2b9a162221f73f2b148425ff47d
YeaChan05 <qkenrdl05@gmail.com>
2025-06-12 15:39:14 +0900

    feat: validation 조건 추가

--- app-api/src/main/java/org/yechan/dto/request/IssueTokenRequest.java
+++ app-api/src/main/java/org/yechan/dto/request/IssueTokenRequest.java
@@ -1,4 +1,13 @@
 package org.yechan.dto.request;
 
-public record IssueTokenRequest(String email, String password) {
+import jakarta.validation.constraints.NotNull;
+import org.yechan.dto.annotation.Email;
+
+public record IssueTokenRequest(
+        @Email
+        @NotNull(message = "이메일을 입력해주세요.")
+        String email,
+
+        @NotNull(message = "비밀번호를 입력해주세요.")
+        String password) {
 }

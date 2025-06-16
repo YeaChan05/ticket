25732e104d42fecf4c46ddc4f9d26a52663b5572
YeaChan05 <qkenrdl05@gmail.com>
2025-06-16 21:53:03 +0900

    feat: 이메일 및 비밀번호 필드에 대한 NotBlank 검증 추가

--- app-api/src/main/java/org/yechan/dto/request/IssueTokenRequest.java
+++ app-api/src/main/java/org/yechan/dto/request/IssueTokenRequest.java
@@ -1,13 +1,16 @@
 package org.yechan.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.yechan.dto.annotation.Email;
 
 public record IssueTokenRequest(
         @Email
         @NotNull(message = "이메일을 입력해주세요.")
+        @NotBlank(message = "이메일을 입력해주세요.")
         String email,
 
         @NotNull(message = "비밀번호를 입력해주세요.")
+        @NotBlank(message = "비밀번호를 입력해주세요.")
         String password) {
 }

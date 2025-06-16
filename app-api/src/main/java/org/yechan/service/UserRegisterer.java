b677b9a5b42d988b133b31bc5b9cf8a4fb0e6f75
YeaChan05 <qkenrdl05@gmail.com>
2025-06-16 23:18:54 +0900

    refactor: 사용자 등록 로직을 UserPersister 및 UserValidator로 분리

--- app-api/src/main/java/org/yechan/service/UserRegisterer.java
+++ app-api/src/main/java/org/yechan/service/UserRegisterer.java
@@ -2,66 +2,35 @@ package org.yechan.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.yechan.api.port.UserRegisterUseCase;
 import org.yechan.dto.request.UserRegisterRequest;
 import org.yechan.dto.response.RegisterSuccessResponse;
-import org.yechan.error.UserErrorCode;
-import org.yechan.error.exception.UserException;
-import org.yechan.repository.UserRepository;
 
 @Slf4j
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserRegisterer implements UserRegisterUseCase {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final UserPersister userPersister;
+    private final UserValidator userValidator;
+
 
     @Override
+    @Transactional
     public RegisterSuccessResponse registerUser(final UserRegisterRequest request) {
-        String email = request.email();
-        String name = request.name();
-        String password = request.password();
-        String phone = request.phone();
         // 이메일 중복 검증
-        validateEmailUniqueness(email);
+        userValidator.validateEmailUniqueness(request.email());
 
         // 이름 중복 검증
-        validateUsernameUniqueness(name);
+        userValidator.validateUsernameUniqueness(request.username());
 
         // 전화번호 중복 검증
-        validatePhoneNumberUniqueness(phone);
+        userValidator.validatePhoneNumberUniqueness(request.phone());
 
         // 저장
-        registerVerifiedUser(name, email, password, phone);
+        userPersister.registerVerifiedUser(request.username(), request.email(), request.password(), request.phone());
 
-        return new RegisterSuccessResponse(name, email);
-    }
-
-    @Transactional
-    protected void registerVerifiedUser(final String name, final String email, final String password, final String phone) {
-        String encryptedPassword = passwordEncoder.encode(password);
-        userRepository.insertUser(name, email, encryptedPassword, phone);
-    }
-
-    protected void validatePhoneNumberUniqueness(final String phone) {
-        if (userRepository.existsByPhone(phone)) {
-            throw new UserException("User with phone " + phone + " already exists.", UserErrorCode.PHONE_DUPLICATED);
-        }
-    }
-
-    protected void validateEmailUniqueness(final String email) {
-        if (userRepository.existsByEmail(email)) {
-            throw new UserException("User with email " + email + " already exists.", UserErrorCode.EMAIL_DUPLICATED);
-        }
-    }
-
-    protected void validateUsernameUniqueness(final String name) {
-        if (userRepository.existsByName(name)) {
-            throw new UserException("User with username " + name + " already exists.", UserErrorCode.NAME_DUPLICATED);
-        }
+        return new RegisterSuccessResponse(request.username(), request.email());
     }
 }
---
+++ app-api/src/main/java/org/yechan/service/UserValidator.java
@@ -0,0 +1,33 @@
+package org.yechan.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.yechan.error.UserErrorCode;
+import org.yechan.error.exception.UserException;
+import org.yechan.repository.UserRepository;
+
+@Component
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserValidator {
+    private final UserRepository userRepository;
+
+    public void validatePhoneNumberUniqueness(final String phone) {
+        if (userRepository.existsByPhone(phone)) {
+            throw new UserException("User with phone " + phone + " already exists.", UserErrorCode.PHONE_DUPLICATED);
+        }
+    }
+
+    public void validateEmailUniqueness(final String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new UserException("User with email " + email + " already exists.", UserErrorCode.EMAIL_DUPLICATED);
+        }
+    }
+
+    public void validateUsernameUniqueness(final String name) {
+        if (userRepository.existsByName(name)) {
+            throw new UserException("User with username " + name + " already exists.", UserErrorCode.NAME_DUPLICATED);
+        }
+    }
+}

da6c70b0335ddb59c7848d6169a2f7d54cbfe6ec
YeaChan05 <qkenrdl05@gmail.com>
2025-06-18 21:51:17 +0900

    test: '판매자 회원가입 요청은 성공적으로 처리된다' 테스트

---
+++ app-api/src/test/java/org/yechan/api/seller/POST_specs.java
@@ -0,0 +1,49 @@
+package org.yechan.api.seller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.yechan.testdata.EmailGenerator.generateEmail;
+import static org.yechan.testdata.PasswordGenerator.generatePassword;
+import static org.yechan.testdata.PhoneNumberGenerator.generatePhone;
+import static org.yechan.testdata.UsernameGenerator.generateUsername;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.yechan.config.IntegrationTest;
+import org.yechan.dto.request.HostRegisterRequest;
+import org.yechan.fixture.TestFixture;
+
+@IntegrationTest
+@DisplayName("POST /api/v1/seller")
+public class POST_specs {
+    @Test
+    void 판매자_회원가입_요청은_성공적으로_처리된다(
+            @Autowired TestFixture fixture
+    ) {
+        // Arrange
+        var hostname = generateUsername();
+        var email = generateEmail();
+        var phone = generatePhone();
+        var password = generatePassword();
+        var request = new HostRegisterRequest(
+                hostname,
+                email,
+                phone,
+                password
+        );
+
+        // Act
+        fixture.post(
+                        "/api/v1/sellers/sign-up",
+                        request,
+                        null
+                )
+                .exchange(Void.class)
+                .onSuccess(
+                        // Assert
+                        response -> {
+                            assertThat(response.getStatus()).isEqualTo("SUCCESS");
+                        }
+                );
+    }
+}

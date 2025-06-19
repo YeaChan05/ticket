08c147e5cd072a5761f2ac30b4517c45b5776ad9
YeaChan05 <qkenrdl05@gmail.com>
2025-06-19 15:32:00 +0900

    feat: 엔드포인트 요청 및 응답 방식 정의

--- app-api/src/main/java/org/yechan/api/SellerController.java
+++ app-api/src/main/java/org/yechan/api/SellerController.java
@@ -2,13 +2,23 @@ package org.yechan.api;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yechan.api.port.SellerRegisterUseCase;
+import org.yechan.dto.request.SellerRegisterRequest;
+import org.yechan.dto.response.SuccessfulSellerRegistrationResponse;
 
 @RestController
 @RequestMapping("/api/v1/sellers")
-public record SellerController() {
+public record SellerController(SellerRegisterUseCase sellerRegisterUseCase) {
     @PostMapping("/sign-up")
-    public void registerSeller() {
+    public SuccessfulSellerRegistrationResponse registerSeller(@RequestBody SellerRegisterRequest request) {
+        return sellerRegisterUseCase.registerSeller(
+                request.sellerName(),
+                request.email(),
+                request.contact(),
+                request.password()
+        );
     }
 }

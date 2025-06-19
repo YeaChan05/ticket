3e3981d8cfcd4e5ace0a3362381089739b9ef942
YeaChan05 <qkenrdl05@gmail.com>
2025-06-19 15:34:58 +0900

    feat: 판매자 등록 비즈니스 로직 구현

---
+++ app-api/src/main/java/org/yechan/service/SellerRegisterer.java
@@ -0,0 +1,34 @@
+package org.yechan.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yechan.api.port.SellerRegisterUseCase;
+import org.yechan.dto.response.SuccessfulSellerRegistrationResponse;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SellerRegisterer implements SellerRegisterUseCase {
+    private final SellerPersister sellerPersister;
+    private final SellerValidator sellerValidator;
+    @Override
+    public SuccessfulSellerRegistrationResponse registerSeller(String sellerName,
+                                                               String email,
+                                                               String contact,
+                                                               String password) {
+        // 이메일 중복 검증
+        sellerValidator.validateEmailUniqueness(email);
+
+        // 판매자 이름 중복 검증
+        sellerValidator.validateSellerNameUniqueness(sellerName);
+
+        // 판매자 전화번호 중복 검증
+        sellerValidator.validateContactUniqueness(contact);
+
+        // 판매자 정보 저장
+        sellerPersister.registerVerifiedSeller(sellerName, email, contact, password);
+
+        return new SuccessfulSellerRegistrationResponse(sellerName, email);
+    }
+}

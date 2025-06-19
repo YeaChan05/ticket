b5be82d1840a472ba1c1742f28e934f9e64aa991
YeaChan05 <qkenrdl05@gmail.com>
2025-06-19 15:58:58 +0900

    feat: 에러코드 수정

--- app-api/src/main/java/org/yechan/error/SellerErrorCode.java
+++ app-api/src/main/java/org/yechan/error/SellerErrorCode.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public enum SellerErrorCode implements ErrorCode {
-    DUPLICATED_SELLER_NAME("SELLER-001","판매자 이름이 이미 존재합니다"),
-    DUPLICATED_EMAIL("SELLER-002","이메일이 이미 존재합니다" ),
+    DUPLICATED_EMAIL("SELLER-001","이메일이 이미 존재합니다" ),
+    DUPLICATED_SELLER_NAME("SELLER-002","판매자 이름이 이미 존재합니다"),
     DUPLICATED_CONTACT("SELLER-003", "연락처가 이미 존재합니다");
 
     private final String code;

50b881be25679d14fca5255844d69eb2869f5b9d
YeaChan05 <qkenrdl05@gmail.com>
2025-06-21 18:54:53 +0900

    feat: query parameter 기능 보충

--- app-api/src/test/java/org/yechan/fixture/RequestExecutor.java
+++ app-api/src/test/java/org/yechan/fixture/RequestExecutor.java
@@ -1,13 +1,17 @@
 package org.yechan.fixture;
 
+import static java.util.Objects.requireNonNull;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.RequestEntity.BodyBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.yechan.config.response.ApiResponse;
 
 public class RequestExecutor {
@@ -16,17 +20,24 @@ public class RequestExecutor {
     private final HttpMethod method;
     private final String url;
     private final Object body;
-    private final Object[] uriVariables;
     private final String token;
+    private Map<String, Object> queryParams;
 
-    RequestExecutor(TestFixture fixture, HttpMethod method, String url, Object body, String token,
-                    Object... uriVariables) {
+    RequestExecutor(TestFixture fixture, HttpMethod method, String url, Object body, String token) {
         this.fixture = fixture;
         this.method = method;
         this.url = url;
         this.body = body;
         this.token = token;
-        this.uriVariables = uriVariables;
+    }
+
+    public RequestExecutor queryParam(String key, Object value) {
+        if (queryParams == null) {
+            this.queryParams = new HashMap<>();
+            this.queryParams.put(key, value);
+        }
+        queryParams.put(key, value);
+        return this;
     }
 
     public <T> TestResult<T> exchange(Class<T> responseDataClass) {
@@ -41,7 +52,16 @@ public class RequestExecutor {
     }
 
     private RequestEntity<?> buildRequestEntity() {
-        BodyBuilder builder = RequestEntity.method(method, url, uriVariables);
+        String finalUrl = url;
+        BodyBuilder builder;
+        if (queryParams != null && !queryParams.isEmpty()) {
+            UriComponentsBuilder ucb = UriComponentsBuilder.fromUriString(url);
+            queryParams.forEach(ucb::queryParam);
+            finalUrl = ucb.build().toUriString();
+            builder = RequestEntity.method(method, finalUrl, requireNonNull(queryParams));
+        } else {
+            builder = RequestEntity.method(method, finalUrl);
+        }
         builder.header("Content-Type", APPLICATION_JSON_VALUE);
         if (token != null && !token.isBlank()) {
             builder.header("Authorization", "Bearer " + token);

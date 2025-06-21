61adf26ae999b24e68e309bfc55db5846c9f5b91
YeaChan05 <qkenrdl05@gmail.com>
2025-06-21 18:54:33 +0900

    feat: 테스트 도구 강화

--- app-api/src/test/java/org/yechan/fixture/TestFixture.java
+++ app-api/src/test/java/org/yechan/fixture/TestFixture.java
@@ -3,7 +3,10 @@ package org.yechan.fixture;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import groovy.util.logging.Slf4j;
 import jakarta.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.ResolvableType;
@@ -11,26 +14,27 @@ import org.springframework.http.HttpMethod;
 import org.yechan.config.response.ApiResponse;
 import org.yechan.config.response.ErrorResponse;
 
+@Slf4j
 public record TestFixture(
         TestRestTemplate client,
         ObjectMapper objectMapper
 ) {
 
+    private static final Logger log = LoggerFactory.getLogger(TestFixture.class);
+
     public RequestExecutor get(
             String url,
-            @Nullable String token,
-            Object... uriVariables
+            @Nullable String token
     ) {
-        return new RequestExecutor(this, HttpMethod.GET, url, null,token, uriVariables);
+        return new RequestExecutor(this, HttpMethod.GET, url, null, token);
     }
 
     public RequestExecutor post(
             String url,
             Object requestBody,
-            @Nullable String token,
-            Object... uriVariables
+            @Nullable String token
     ) {
-        return new RequestExecutor(this, HttpMethod.POST, url, requestBody,token, uriVariables);
+        return new RequestExecutor(this, HttpMethod.POST, url, requestBody, token);
     }
 
     <T> TestResult<T> parseResponseBody(
@@ -48,7 +52,9 @@ public record TestFixture(
         try {
             return objectMapper.readValue(responseBody, ErrorResponse.class);
         } catch (JsonProcessingException ex) {
-            throw new RuntimeException("Failed to parse response body into any known type (ApiResponse or ErrorResponse)", ex);
+            log.error("response {}", responseBody );
+            throw new RuntimeException(
+                    "Failed to parse response body into any known type (ApiResponse or ErrorResponse)", ex);
         }
     }
 

06f31769f2826427850c9bb8bfbeca57d44ff14a
YeaChan05 <qkenrdl05@gmail.com>
2025-06-21 14:28:20 +0900

    feat: 엔드포인트 구현

---
+++ app-api/src/main/java/org/yechan/api/ShowController.java
@@ -0,0 +1,19 @@
+package org.yechan.api;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yechan.dto.request.ShowRegisterRequest;
+import org.yechan.dto.response.ShowRegisterResponse;
+
+@RequestMapping("/api/v1/show")
+@RestController
+public record ShowController() {
+
+    @PostMapping
+    public ShowRegisterResponse registerShow(@Valid @RequestBody ShowRegisterRequest request) {
+        return new ShowRegisterResponse(null,null);
+    }
+}

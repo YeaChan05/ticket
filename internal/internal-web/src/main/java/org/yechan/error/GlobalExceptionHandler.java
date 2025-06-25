7f6cb9f501c7dd8fd09db111fee8e65cb20c9364
YeaChan05 <qkenrdl05@gmail.com>
2025-06-25 10:08:07 +0900

    feat: 추상화된 예외 처리기 구현

--- internal/internal-web/src/main/java/org/yechan/error/GlobalExceptionHandler.java
+++ internal/internal-web/src/main/java/org/yechan/error/GlobalExceptionHandler.java
@@ -12,14 +12,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @Component
-@RestControllerAdvice(basePackages = "org.yechan.api")
-//TODO 2025 06 01 15:27:37 : 애플리케이션 모듈 구조에 따라 basePackages를 변경해야 할 수도 있음
+@RestControllerAdvice(basePackages = "org.yechan.api")//TODO 2025 06 01 15:27:37 : 애플리케이션 모듈 구조에 따라 basePackages를 변경해야 할 수도 있음
 public class GlobalExceptionHandler {
 
     private static final String LOG_FORMAT = "Exception occurred: {} - Error Code: {}, Message: {}";
 
-    @ExceptionHandler(BusinessException.class)
-    protected ErrorCode handleBusinessException(BusinessException e) {
+    @ExceptionHandler(value = {AbstractApplicationException.class})
+    protected ErrorCode handleBusinessException(AbstractApplicationException e) {
         ErrorCode errorCode = e.getErrorCode();
         logException(e, errorCode);
         return errorCode;

f440e0585d54834946f96514fa6d699370be5fdc
YeaChan05 <qkenrdl05@gmail.com>
2025-06-25 11:22:44 +0900

    feat: 공연정보 생성기 구현

---
+++ app-api/src/test/java/org/yechan/testdata/ShowInfoGenerator.java
@@ -0,0 +1,40 @@
+package org.yechan.testdata;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.yechan.dto.request.ShowScheduleRegisterRequest;
+import org.yechan.dto.request.TicketGradeRequest;
+
+public class ShowInfoGenerator {
+    public static UUID generateHallId() {
+        return UUID.randomUUID();
+    }
+
+    public static String generateUrl() {
+        return "https://example.com/show/" + UUID.randomUUID();
+    }
+
+    public static String generateDescription() {
+        return "This is a sample description for the show.";
+    }
+
+    public static String generateTitle() {
+        return "Sample Show Title " + UUID.randomUUID();
+    }
+
+    public static ShowScheduleRegisterRequest generateSchedule(int plusDay) {
+        return new ShowScheduleRegisterRequest(
+                LocalDateTime.now().plusDays(plusDay),
+                LocalDateTime.now().plusDays(plusDay).plusHours(2)
+        );
+    }
+
+    public static TicketGradeRequest generateTicketGrade(String grade) {
+        return new TicketGradeRequest(
+                grade,
+                BigDecimal.valueOf(10000 + 1000.0 * Math.random() * 900),
+                (int) (50 + 50 * Math.random())
+        );
+    }
+}

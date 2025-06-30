018d184d8a12a0460b8f63b71e5e338d1bc42a23
YeaChan05 <qkenrdl05@gmail.com>
2025-06-30 19:15:54 +0900

    feat: 에러 코드 추가

--- internal/internal-web/src/main/java/org/yechan/error/GlobalErrorCode.java
+++ internal/internal-web/src/main/java/org/yechan/error/GlobalErrorCode.java
@@ -11,7 +11,8 @@ public enum GlobalErrorCode implements ErrorCode {
     INVALID_SIGNATURE("INVALID_SIGNATURE", "유효하지 않은 서명입니다."),
     MALFORMED_TOKEN("MALFORMED_TOKEN", "잘못된 토큰 형식입니다."),
     UNSUPPORTED_TOKEN("UNSUPPORTED_TOKEN", "지원하지 않는 토큰입니다."),
-    EMPTY_TOKEN("EMPTY_TOKEN", "토큰이 비어 있습니다.");
+    EMPTY_TOKEN("EMPTY_TOKEN", "토큰이 비어 있습니다."),
+    UNSUPPORTED_SIGNATURE_ALGORITHM("UNSUPPORTED_SIGNATURE_ALGORITHM","지원되지 않는 서명 알고리즘입니다." );
 
     private final String code;
     private final String message;

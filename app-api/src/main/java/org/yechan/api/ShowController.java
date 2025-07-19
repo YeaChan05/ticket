37b2f5c71dbf7c986dfadb518dde5278d5ce0d10
YeaChan05 <qkenrdl05@gmail.com>
2025-07-19 14:27:08 +0900

    feat: 공연 등록 유스케이스 추가
    공연 등록 시 판매자 검증 및 요청 데이터 처리 로직 구현.

--- app-api/src/main/java/org/yechan/api/ShowController.java
+++ app-api/src/main/java/org/yechan/api/ShowController.java
@@ -1,19 +1,27 @@
 package org.yechan.api;
 
+import static org.yechan.api.port.AuthUser.UserType.SELLER;
+
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yechan.api.port.AuthUser;
+import org.yechan.api.port.ShowRegisterUseCase;
 import org.yechan.dto.request.ShowRegisterRequest;
 import org.yechan.dto.response.ShowRegisterResponse;
+import org.yechan.entity.Seller;
 
 @RequestMapping("/api/v1/shows")
 @RestController
-public record ShowController() {
+public record ShowController(
+        ShowRegisterUseCase showRegisterUseCase
+) {
 
     @PostMapping
-    public ShowRegisterResponse registerShow(@Valid @RequestBody ShowRegisterRequest request) {
-        return new ShowRegisterResponse(null,null);
+    public ShowRegisterResponse registerShow(@Valid @RequestBody ShowRegisterRequest request,
+                                             @AuthUser(type = SELLER) Seller seller) {
+        return showRegisterUseCase.register(request, seller);
     }
 }

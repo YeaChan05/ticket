2a96eef30708eb88d7aa57f4b9c9556c9fceb7ca
YeaChan05 <qkenrdl05@gmail.com>
2025-07-19 14:29:46 +0900

    test: '성공적인 공연 정보 등록 후 redirectUrl이 반환되어야 한다' 테스트
    불필요한 메서드 및 의존성 제거, Token 생성 로직 간소화. `성공적인_공연_정보_등록_후_redirectUrl이_반환되어야_한다` 테스트 케이스 추가.

--- app-api/src/test/java/org/yechan/api/show/POST_specs.java
+++ app-api/src/test/java/org/yechan/api/show/POST_specs.java
@@ -2,17 +2,12 @@ package org.yechan.api.show;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.yechan.testdata.CategoryGenerator.pickAnyCategory;
-import static org.yechan.testdata.EmailGenerator.generateEmail;
-import static org.yechan.testdata.PasswordGenerator.generatePassword;
-import static org.yechan.testdata.PhoneNumberGenerator.generatePhone;
 import static org.yechan.testdata.ShowInfoGenerator.generateDescription;
 import static org.yechan.testdata.ShowInfoGenerator.generateHallId;
 import static org.yechan.testdata.ShowInfoGenerator.generateSchedule;
 import static org.yechan.testdata.ShowInfoGenerator.generateTicketGrade;
 import static org.yechan.testdata.ShowInfoGenerator.generateTitle;
 import static org.yechan.testdata.ShowInfoGenerator.generateUrl;
-import static org.yechan.testdata.TestTokenGenerator.generateToken;
-import static org.yechan.testdata.UsernameGenerator.generateUsername;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,11 +15,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.yechan.config.IntegrationTest;
-import org.yechan.dto.request.IssueTokenRequest;
-import org.yechan.dto.request.SellerRegisterRequest;
 import org.yechan.dto.request.ShowRegisterRequest;
 import org.yechan.dto.response.ShowRegisterResponse;
-import org.yechan.dto.response.SuccessfulSellerRegisterResponse;
 import org.yechan.entity.Seller;
 import org.yechan.fixture.TestFixture;
 
@@ -57,8 +49,7 @@ public class POST_specs {
         );
 
         // Act
-        var issueTokenRequest = generateSeller(fixture);
-        var token = generateToken(Seller.class, fixture, issueTokenRequest);
+        var token = fixture.generateToken(Seller.class);
         fixture.post(
                         "/api/v1/shows",
                         request,
@@ -73,25 +64,46 @@ public class POST_specs {
                 );
     }
 
-    private IssueTokenRequest generateSeller(TestFixture fixture) {
-        var email = generateEmail();
-        var password = generatePassword();
-        fixture.post(
-                        "/api/v1/sellers/sign-up",
-                        new SellerRegisterRequest(
-                                generateUsername(),
-                                email,
-                                password,
-                                generatePhone()
-                        ),
-                        null
+    @Test
+    void 성공적인_공연_정보_등록_후_redirectUrl이_반환되어야_한다(
+            @Autowired TestFixture fixture
+    ) {
+        // Arrange
+        var plusDay = (int) (Math.random() * 30);
+        var request = new ShowRegisterRequest(
+                generateTitle(),
+                generateDescription(),
+                pickAnyCategory(),
+                generateUrl(),
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(1),
+                generateHallId(),
+                List.of(
+                        generateSchedule(plusDay),
+                        generateSchedule(plusDay + 3)
+                ),
+                List.of(
+                        generateTicketGrade("VIP"),
+                        generateTicketGrade("RVIP")
                 )
-                .exchange(SuccessfulSellerRegisterResponse.class)
-                .getApiResponse();
-
-        return new IssueTokenRequest(
-                email,
-                password
         );
+
+        // Act
+        var token = fixture.generateToken(Seller.class);
+        fixture.post(
+                        "/api/v1/shows",
+                        request,
+                        token
+                )
+                .exchange(ShowRegisterResponse.class)
+                .onSuccess(
+                        // Assert
+                        response -> {
+                            assertThat(response.getData().redirectUrl())
+                                    .isNotNull()
+                                    .contains("/api/v1/shows/");
+                        }
+                );
     }
+
 }

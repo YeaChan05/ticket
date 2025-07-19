a4fdf86949be47b20320cb4066a03a4e6782fe5a
YeaChan05 <qkenrdl05@gmail.com>
2025-07-19 15:30:49 +0900

    test: '공연 정보 등록 후, 등록된 공연 정보가 데이터베이스에 저장되어야 한다' 테스트
    `generateShowRegisterRequest` 유틸 메서드 추가 및 테스트 케이스 구현.

--- app-api/src/test/java/org/yechan/api/show/POST_specs.java
+++ app-api/src/test/java/org/yechan/api/show/POST_specs.java
@@ -5,7 +5,6 @@ import static org.yechan.testdata.CategoryGenerator.pickAnyCategory;
 import static org.yechan.testdata.ShowInfoGenerator.generateDescription;
 import static org.yechan.testdata.ShowInfoGenerator.generateHallId;
 import static org.yechan.testdata.ShowInfoGenerator.generateSchedule;
-import static org.yechan.testdata.ShowInfoGenerator.generateTicketGrade;
 import static org.yechan.testdata.ShowInfoGenerator.generateTitle;
 import static org.yechan.testdata.ShowInfoGenerator.generateUrl;
 
@@ -19,18 +18,15 @@ import org.yechan.dto.request.ShowRegisterRequest;
 import org.yechan.dto.response.ShowRegisterResponse;
 import org.yechan.entity.Seller;
 import org.yechan.fixture.TestFixture;
+import org.yechan.repository.JpaShowRepository;
+import org.yechan.testdata.ShowInfoGenerator;
 
 @IntegrationTest
 @DisplayName("POST /api/v1/shows")
 public class POST_specs {
 
-    @Test
-    void 정상적인_공연_정보_등록은_성공적으로_이루어져야_한다(
-            @Autowired TestFixture fixture
-    ) {
-        // Arrange
-        var plusDay = (int) (Math.random() * 30);
-        var request = new ShowRegisterRequest(
+    private static ShowRegisterRequest generateShowRegisterRequest(List<String> grades, int plusDay) {
+        return new ShowRegisterRequest(
                 generateTitle(),
                 generateDescription(),
                 pickAnyCategory(),
@@ -42,11 +38,19 @@ public class POST_specs {
                         generateSchedule(plusDay),
                         generateSchedule(plusDay + 3)
                 ),
-                List.of(
-                        generateTicketGrade("VIP"),
-                        generateTicketGrade("RVIP")
-                )
+                grades.stream()
+                        .map(ShowInfoGenerator::generateTicketGrade)
+                        .toList()
         );
+    }
+
+    @Test
+    void 정상적인_공연_정보_등록은_성공적으로_이루어져야_한다(
+            @Autowired TestFixture fixture
+    ) {
+        // Arrange
+        var grades = List.of("VIP", "RVIP");
+        var request = generateShowRegisterRequest(grades, (int) (Math.random() * 30));
 
         // Act
         var token = fixture.generateToken(Seller.class);
@@ -69,24 +73,8 @@ public class POST_specs {
             @Autowired TestFixture fixture
     ) {
         // Arrange
-        var plusDay = (int) (Math.random() * 30);
-        var request = new ShowRegisterRequest(
-                generateTitle(),
-                generateDescription(),
-                pickAnyCategory(),
-                generateUrl(),
-                LocalDateTime.now(),
-                LocalDateTime.now().plusDays(1),
-                generateHallId(),
-                List.of(
-                        generateSchedule(plusDay),
-                        generateSchedule(plusDay + 3)
-                ),
-                List.of(
-                        generateTicketGrade("VIP"),
-                        generateTicketGrade("RVIP")
-                )
-        );
+        var grades = List.of("VIP", "RVIP");
+        var request = generateShowRegisterRequest(grades, (int) (Math.random() * 30));
 
         // Act
         var token = fixture.generateToken(Seller.class);
@@ -106,4 +94,35 @@ public class POST_specs {
                 );
     }
 
+    @Test
+    @DisplayName("공연 정보 등록 후, 등록된 공연 정보가 데이터베이스에 저장되어야 한다")
+    void 공연_정보_등록_후_등록된_공연_정보가_데이터베이스에_저장되어야_한다(
+            @Autowired TestFixture fixture,
+            @Autowired JpaShowRepository showRepository
+    ) {
+        // Arrange
+        var grades = List.of("VIP", "RVIP");
+        var request = generateShowRegisterRequest(grades, (int) (Math.random() * 30));
+
+        // Act
+        var token = fixture.generateToken(Seller.class);
+        fixture.post(
+                        "/api/v1/shows",
+                        request,
+                        token
+                )
+                .exchange(ShowRegisterResponse.class)
+                .onSuccess(
+                        // Assert
+                        response -> {
+                            assertThat(showRepository.findAll())
+                                    .first()
+                                    .satisfies(
+                                            show -> assertThat(show.getCategory()).isEqualTo(request.category()),
+                                            show -> assertThat(show.getTitle()).isEqualTo(request.title()),
+                                            show -> assertThat(show.getDescription()).isEqualTo(request.description()),
+                                            show -> assertThat(show.getHallId()).isEqualTo(request.hallId())
+                                    );
+                        });
+    }
 }

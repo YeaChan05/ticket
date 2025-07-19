6b448383cd09d92f82cb11893b14d1e1f0c71250
YeaChan05 <qkenrdl05@gmail.com>
2025-07-19 14:28:52 +0900

    fix: TokenProvider 리턴 값 수정
    `claims.get("email")` 을 사용하여 인증 정보 설정.

--- internal/internal-web/src/main/java/org/yechan/config/security/TokenProvider.java
+++ internal/internal-web/src/main/java/org/yechan/config/security/TokenProvider.java
@@ -86,6 +86,6 @@ public class TokenProvider {
                 .map(SimpleGrantedAuthority::new)
                 .toList();
 
-        return new UsernamePasswordAuthenticationToken(claims.getSubject(), null, authorities);
+        return new UsernamePasswordAuthenticationToken(claims.get("email"), null, authorities);
     }
 }
